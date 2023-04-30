8eb300b6fadfa48a6ffca3d30193d4436db9ca1d
Ariel Vergara <avergara@intercept.cloud>
2023-04-30 09:22:04 +0200

    Restructuring project folders + refactoring code

--- Adventours/DataAccess/Repositories/TourRepository.cs
+++ Adventours/DataAccess/Repositories/TourRepository.cs
@@ -6,20 +6,17 @@ namespace DataAccess.Repositories;
 
 public class TourRepository : RepositoryBase<TourEntity>, ITourRepository
 {
-    private readonly AdventourContext _context;
-
     public TourRepository(AdventourContext context) : base(context)
     {
-        _context = context;
     }
 
     public async Task<ICollection<TourEntity>> GetAllByTravelerId(int userId) => 
-        await _context.Tours
+        await DbSet
             .Where(tour => tour.Travelers != null && tour.Travelers.Any(traveler => traveler.UserId == userId))
             .ToListAsync();
 
     public async Task<ICollection<TourEntity>> GetAllByTourLeaderId(int tourLeaderId) => 
-        await _context.Tours
+        await DbSet
             .Where(tour => tour.TourLeader.Id == tourLeaderId)
             .ToListAsync();
 }
