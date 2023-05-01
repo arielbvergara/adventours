b06b83e6e1527f006a5991c034693085ae435a2d
Ariel Vergara <avergara@intercept.cloud>
2023-05-01 08:38:18 +0200

    Adding tourMapper and tourResource

---
+++ Adventours/Adventours.API/Features/Tours/TourMapper.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Entities;
+
+namespace Adventours.API.Features.Tours;
+
+public static class TourMapper
+{
+    public static TourResource ToResource(this TourEntity tourEntity) => 
+        new TourResource(tourEntity.Id, 
+            tourEntity.Title, 
+            tourEntity.Description, 
+            tourEntity.TourLeader);
+
+    public static ICollection<TourResource> ToResources(this ICollection<TourEntity> tourEntities) =>
+        tourEntities.Select(ToResource).ToList();
+}
---
+++ Adventours/Adventours.API/Features/Tours/TourResource.cs
@@ -0,0 +1,5 @@
+﻿using DataAccess.Entities;
+
+namespace Adventours.API.Features.Tours;
+
+public record TourResource(int TourId, string Title, string Description, UserEntity User);
